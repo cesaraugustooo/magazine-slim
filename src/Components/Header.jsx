22b166b671f113110e63035befdf5d4331a26ffd
CESAR AUGUSTO ESQUIÇACTO FERREIRA <cesar.ferreira@portalsesisp.org.br>
2025-03-14 15:48:56 -0300

    commit inicial

---
+++ src/Components/Header.jsx
@@ -0,0 +1,12 @@
+import react from "react";
+import '../App.css';
+import Menu from "./Menu";
+
+export default function Header(){
+
+    return(
+        <div className="header">
+            <Menu></Menu>
+        </div>
+    );
+}
---
+++ src/Components/Menu.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import menu from '../assets/menu.png'
+
+
+export default function Menu(){
+    return(
+        <div className="menu-div" >
+            <img className="menu" src={menu}></img>
+        </div>
+    );
+
+   
+}
