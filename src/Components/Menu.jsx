59ed4a5f90d64a6c8ed4ce8c7d652e44bc24947c
Cesar Augusto <cesaraugusto23121989@gmail.com>
2025-03-18 23:31:41 -0300

    Add components

--- src/Components/Menu.jsx
+++ src/Components/Menu.jsx
@@ -1,31 +1,59 @@
 import React from "react";
 import menu from '../assets/menu.png';
 import logo from '../assets/image-removebg-preview (10).png';
+import { BrowserRouter, Link } from "react-router-dom";
 
-export default function Menu(){
-    
-    return(
+export default function Menu() {
+    return (
         <>
-        <div className="menu-div" >
-           <svg xmlns="http://www.w3.org/2000/svg" width="45" height="45" fill="currentColor" class="bi bi-list-ul" viewBox="0 0 16 16">
-    <path fill-rule="evenodd" d="M5 11.5a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5m-3 1a1 1 0 1 0 0-2 1 1 0 0 0 0 2m0 4a1 1 0 1 0 0-2 1 1 0 0 0 0 2m0 4a1 1 0 1 0 0-2 1 1 0 0 0 0 2"/>
-    </svg>
-    </div>
-    <div className="menu-div" id="um">
-
-    <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-gear" viewBox="0 0 16 16">
+            <div className="menu-div">
+                <a data-bs-toggle="offcanvas" href="#offcanvasExample" role="button" aria-controls="offcanvasExample">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="45" height="45" fill="currentColor" className="bi bi-list-ul" viewBox="0 0 16 16">
+                        <path fillRule="evenodd" d="M5 11.5a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5m-3 1a1 1 0 1 0 0-2 1 1 0 0 0 0 2m0 4a1 1 0 1 0 0-2 1 1 0 0 0 0 2m0 4a1 1 0 1 0 0-2 1 1 0 0 0 0 2" />
+                    </svg>
+                </a>
+            </div>
+            <div className="menu-div" id="um">
+            <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-gear" viewBox="0 0 16 16">
     <path d="M8 4.754a3.246 3.246 0 1 0 0 6.492 3.246 3.246 0 0 0 0-6.492M5.754 8a2.246 2.246 0 1 1 4.492 0 2.246 2.246 0 0 1-4.492 0"/>
     <path d="M9.796 1.343c-.527-1.79-3.065-1.79-3.592 0l-.094.319a.873.873 0 0 1-1.255.52l-.292-.16c-1.64-.892-3.433.902-2.54 2.541l.159.292a.873.873 0 0 1-.52 1.255l-.319.094c-1.79.527-1.79 3.065 0 3.592l.319.094a.873.873 0 0 1 .52 1.255l-.16.292c-.892 1.64.901 3.434 2.541 2.54l.292-.159a.873.873 0 0 1 1.255.52l.094.319c.527 1.79 3.065 1.79 3.592 0l.094-.319a.873.873 0 0 1 1.255-.52l.292.16c1.64.893 3.434-.902 2.54-2.541l-.159-.292a.873.873 0 0 1 .52-1.255l.319-.094c1.79-.527 1.79-3.065 0-3.592l-.319-.094a.873.873 0 0 1-.52-1.255l.16-.292c.893-1.64-.902-3.433-2.541-2.54l-.292.159a.873.873 0 0 1-1.255-.52zm-2.633.283c.246-.835 1.428-.835 1.674 0l.094.319a1.873 1.873 0 0 0 2.693 1.115l.291-.16c.764-.415 1.6.42 1.184 1.185l-.159.292a1.873 1.873 0 0 0 1.116 2.692l.318.094c.835.246.835 1.428 0 1.674l-.319.094a1.873 1.873 0 0 0-1.115 2.693l.16.291c.415.764-.42 1.6-1.185 1.184l-.291-.159a1.873 1.873 0 0 0-2.693 1.116l-.094.318c-.246.835-1.428.835-1.674 0l-.094-.319a1.873 1.873 0 0 0-2.692-1.115l-.292.16c-.764.415-1.6-.42-1.184-1.185l.159-.291A1.873 1.873 0 0 0 1.945 8.93l-.319-.094c-.835-.246-.835-1.428 0-1.674l.319-.094A1.873 1.873 0 0 0 3.06 4.377l-.16-.292c-.415-.764.42-1.6 1.185-1.184l.292.159a1.873 1.873 0 0 0 2.692-1.115z"/>
     </svg>
-    </div>
-    <div className="logo">
-        <img src={logo} alt="" />
-    </div>
-    <div className="menu-open">
-            
-     </div>
-    </>
-    );
+            </div>
+            <div className="logo">
+                <img src={logo} alt="Logo" />
+            </div>
 
-   
+            <div className="offcanvas offcanvas-start" tabIndex="-1" id="offcanvasExample" aria-labelledby="offcanvasExampleLabel">
+                <div className="offcanvas-header">
+                    <h5 className="offcanvas-title" id="offcanvasExampleLabel">Menu</h5>
+                    <button type="button" className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                </div>
+                <div className="offcanvas-body">
+                    <div className="column">
+                        <ul className="list-group">
+                            <li className="list-group-item hover"><Link to='/'>Inicio</Link> </li>
+                            <li className="list-group-item hover"><Link to='/postar'>Postar</Link> </li>
+                            <li className="list-group-item hover"><Link to='/confirmar/post'>Confirmar Posts</Link></li>
+                            <li className="list-group-item hover"><Link to='/perfil'>Perfil</Link></li>
+                        </ul>
+                    </div>
+                    <div className="dropdown mt-3">
+                        <button className="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown">
+                            Materias
+                        </button>
+                        <ul className="dropdown-menu">
+                        <li><a className="dropdown-item" href="#">Matematica</a></li>
+                            <li><a className="dropdown-item" href="#">Historia</a></li>
+                            <li><a className="dropdown-item" href="#">Ciencias</a></li>
+                            <li><a className="dropdown-item" href="#">Linguagens</a></li>
+                            <li><a className="dropdown-item" href="#">Quimica</a></li>
+                            <li><a className="dropdown-item" href="#">Fisica</a></li>
+
+
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
 }
