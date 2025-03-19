f92be564b5169d7e1a24683f3a6fff3facda3327
Cesar Augusto <cesaraugusto23121989@gmail.com>
2025-03-18 23:31:20 -0300

    Add components

---
+++ src/Components/Evento.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import evento from '../assets/evento.png';
+
+export default function Evento(){
+    return(
+        <div className="row-event">
+            <img src={evento} alt="" />
+        </div>
+    );
+}
