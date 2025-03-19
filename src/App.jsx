a59d65f15d33be25b92062f9417539f8c77007e3
Cesar Augusto <cesaraugusto23121989@gmail.com>
2025-03-18 23:30:54 -0300

    Add routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
 import './App.css';
 import Header from './Components/Header'; 
+import Fluxo from './Screens/Fluxo';
 import Formulario from './Screens/Form';
+import { BrowserRouter, Route, Router, Routes } from 'react-router-dom';
+import Home from './Screens/Home';
 function App() {
   return (
     <div className="App">
-      <Header></Header>
-      <Formulario></Formulario>
+      <BrowserRouter>
+        <Header></Header>
+        <Routes>
+          <Route path='/' element={<Home />}></Route>
+          <Route path='/postar' element={<Formulario></Formulario>}></Route>
+          <Route path='/confirmar/post' element={<Fluxo/>}></Route>
+        </Routes>
+      </BrowserRouter>
+      
     </div>
   );
 }
