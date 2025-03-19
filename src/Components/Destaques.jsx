be93ce85b06ba970c3d2f6fa1b2bcd440c88a46c
Cesar Augusto <cesaraugusto23121989@gmail.com>
2025-03-18 23:30:17 -0300

    Add screen

---
+++ src/Components/Destaques.jsx
@@ -0,0 +1,40 @@
+import React,{useState,useEffect} from "react";
+import Evento from "../assets/evento.png";
+
+export default function Destaques(){
+    const [data,innerData] = useState([]);
+    useEffect(()=>{
+        async function getDestaques(){
+            const api = await fetch('http://localhost/RevistaDigital_API/posts');
+    
+            const data = await api.json();
+
+            innerData(data);
+
+        }
+        getDestaques();
+    },[]);
+   
+
+    return(
+        <div className="destaques">
+            <div className="row-destaques">
+                <h3>Destaques</h3>
+                <div className="hr-destaques"></div>
+            </div>
+            <div className="row-noticias" id="noticias-home">
+                {data.slice(2,5).map((post) => 
+                    <div className="card" key={post.id_post}>
+                            <img src={post.foto_post} className="imgCard" alt={post.titulo_post} />
+                            <div className="card-body">
+                                <p className="card-text">{post.titulo_post}</p>
+                            </div>
+                        </div>
+
+                )};
+            </div>
+           
+
+        </div>
+    );
+}
