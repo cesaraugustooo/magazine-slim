81f10cc7c66fa5888d4b4b27e7f9d996603e85f7
Cesar Augusto <cesaraugusto23121989@gmail.com>
2025-03-18 23:32:14 -0300

    Add screen

---
+++ src/Screens/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Evento from "../Components/Evento";
+import Destaques from "../Components/Destaques";
+export default function Home(){
+    return(
+        <>
+            <Evento></Evento>
+            <Destaques></Destaques>
+        </>
+    );
+}
