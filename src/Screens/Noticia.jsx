e4230be9f6ed233aedaf37fe18d73de1a6bec8ac
Cesar Augusto <cesaraugusto23121989@gmail.com>
2025-03-24 22:53:27 -0300

    Nova tela

---
+++ src/Screens/Noticia.jsx
@@ -0,0 +1,12 @@
+import React,{useEffect,useState} from "react";
+
+export default function Noticia(){
+
+    return(
+        <>
+             <div className="titulo">
+            <h3>Categoria</h3>
+        </div>
+        </>
+    );
+}
