389164a9a9f6c749e6e62cbd02368a5df01f141f
Cesar Augusto <cesaraugusto23121989@gmail.com>
2025-03-24 22:53:13 -0300

    novo componente

--- src/Screens/Home.jsx
+++ src/Screens/Home.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Evento from "../Components/Evento";
 import Destaques from "../Components/Destaques";
+import Lembrancas from "../Components/Lembrancas"
 export default function Home(){
     return(
         <>
             <Evento></Evento>
             <Destaques></Destaques>
+            <Lembrancas></Lembrancas>
         </>
     );
 }
